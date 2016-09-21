e4b8916b269e7c26dcc707dddda77ffda0f72ead
Caleb Hawthorne <hawthornec15@sirisaac.net>
2016-09-21 12:16:09 +0100

    Initial Commit

---
+++ PepesComing/App.vb
@@ -0,0 +1,19 @@
+﻿Imports Microsoft.Xna.Framework
+
+
+Public Class Application
+    Shared _game As Game
+
+    Shared Sub Main()
+        Try
+            _game = New Game1()
+            _game.Run()
+        Catch ex As Exception
+            Console.WriteLine("Boo! something went wrong!")
+            Console.WriteLine(ex.Message)
+            Console.WriteLine(ex.InnerException)
+            Console.WriteLine(ex.StackTrace)
+        End Try
+
+    End Sub
+End Class
---
+++ PepesComing/Game.vb
@@ -0,0 +1,51 @@
+﻿Imports Microsoft.Xna.Framework
+Imports Microsoft.Xna.Framework.Graphics
+Imports Microsoft.Xna.Framework.Input
+
+Public Class Game1
+    Inherits Game
+
+    Private _graphicsDeviceManager As GraphicsDeviceManager
+    Private spriteBatch As SpriteBatch
+    Private pepe As Texture2D
+    Private counter As Integer = 0
+
+    Public Sub New()
+        Content.RootDirectory = "Content"
+
+        _graphicsDeviceManager = New GraphicsDeviceManager(Me)
+        _graphicsDeviceManager.IsFullScreen = False
+
+    End Sub
+
+    Protected Overrides Sub Initialize()
+        MyBase.Initialize()
+    End Sub
+
+    Protected Overrides Sub LoadContent()
+        ' Load assets etc in here
+        spriteBatch = New SpriteBatch(GraphicsDevice)
+        pepe = Content.Load(Of Texture2D)("pepe")
+    End Sub
+
+    Protected Overrides Sub UnloadContent()
+        ' Unload assets etc in here
+    End Sub
+
+    Protected Overrides Sub Draw(gameTime As GameTime)
+        GraphicsDevice.Clear(Color.Cornsilk)
+
+        spriteBatch.Begin()
+        spriteBatch.Draw(pepe, New Rectangle(0 + counter, 0, 400, 400), Color.White)
+        spriteBatch.End()
+
+        ' Drawing code goes here
+
+        MyBase.Draw(gameTime)
+    End Sub
+
+    Protected Overrides Sub Update(gameTime As GameTime)
+        counter = counter + 1
+        MyBase.Update(gameTime)
+    End Sub
+End Class
